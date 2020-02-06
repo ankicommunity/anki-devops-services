114647ad043e085652e2398594efa259f4861bde
Kuklin István <kuklinistvan@zoho.com>
2020-02-06 11:27:30 +0100

    ankisyncd 2.1.0 + 2bfccf7f

--- Docker/build.sh
+++ Docker/build.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR"
 git submodule update --init --recursive
-docker build -t anki-sync-server:tsudoku-ankisyncd-2.1.0 .
+docker build -t anki-sync-server:tsudoko-ankisyncd-2.1.0-plus-2bfccf7f .
--- Docker/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-cd "$DIR"
-
-docker run -it \
-       --mount type=bind,source="$DIR/data",target=/app/data \
-       -p 27701:27701 \
-       anki-sync-server:tsudoku-2.1.4 \
