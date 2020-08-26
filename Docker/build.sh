dadfb6bffe407c09c950d9ff70a9d732a3bf484d
Kuklin István Alexander <istvan@codekuklin.com>
2020-08-26 13:53:12 +0200

    Docker tagging

--- Docker/build.sh
+++ Docker/build.sh
@@ -3,4 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR"
 git submodule update --init --recursive
 docker build -t anki-sync-server:tsudoko-ankisyncd-2.1.0-plus-2bfccf7f .
+
+# DockerHub kuklinistvan/
+docker tag anki-sync-server:tsudoko-ankisyncd-2.1.0-plus-2bfccf7f kuklinistvan/anki-sync-server:tsudoko-ankisyncd-2.1.0-plus-2bfccf7f 
 docker tag anki-sync-server:tsudoko-ankisyncd-2.1.0-plus-2bfccf7f kuklinistvan/anki-sync-server:latest 
