493cd2dcc64fd8025bc552124eff301a50c0de58
Kuklin István Alexander <istvan@codekuklin.com>
2020-08-26 13:41:41 +0200

    Healthcheck added

--- Docker/build.sh
+++ Docker/build.sh
@@ -3,3 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR"
 git submodule update --init --recursive
 docker build -t anki-sync-server:tsudoko-ankisyncd-2.1.0-plus-2bfccf7f .
+docker tag anki-sync-server:tsudoko-ankisyncd-2.1.0-plus-2bfccf7f kuklinistvan/anki-sync-server:latest 
