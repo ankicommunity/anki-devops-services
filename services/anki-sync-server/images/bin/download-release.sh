6b4727fef54a6a195ab44f88eba555fd85daf1b8
Shaun Smiley <senorsmile@gmail.com>
2021-10-24 20:44:27 -0700

    add ANKISYNCD_COMMIT This adds the ability to checkout a specific commit of anki-sync-server as well as specify the version of the image to be built.

--- services/anki-sync-server/images/bin/download-release.sh
+++ services/anki-sync-server/images/bin/download-release.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 # file: download-release.sh
 
+set -eu
 
 mkdir -p release
 
 cd release
 
 git clone https://github.com/ankicommunity/anki-sync-server
+
+if [ "${ANKISYNCD_COMMIT}" != 'latest' ]; then
+  cd anki-sync-server
+  git checkout "${ANKISYNCD_COMMIT}"
+  cd ..
+fi
 
 mv anki-sync-server/src/* .
 
