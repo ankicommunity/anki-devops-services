cfe069efd07c9ada9f87115c62be79081a054980
Vikash Kothary <kothary.vikash@gmail.com>
2022-04-20 19:52:50 +0100

    wip: add build information to anki-sync-server via docker labels

--- scripts/build-devops.sh
+++ scripts/build-devops.sh
@@ -2,6 +2,14 @@
 # file: build-devops.sh
 # description: Build all Docker images
 
+git_dirty () {
+	if [[ $(git status --porcelain)  ]]; then 
+		echo "true"; 
+	else 
+		echo "false";
+	fi
+}
+
 build_image () {
 	SERVICE=$1
 
@@ -21,6 +29,23 @@ build_image () {
 }
 
 pushd ${GITCLOUD_DEVOPS_PATH:=.}
+
+echo "--- BUILD_USER_NAME: ${BUILD_USER_NAME}"
+echo "--- BUILD_USER_EMAIL: ${BUILD_USER_EMAIL}"
+export BUILD_DATE=$(date '+%Y-%m-%d')
+echo "--- BUILD_DATE: ${BUILD_DATE}"
+export GIT_REPOSITORY_URL=$(git remote get-url origin)
+echo "--- GIT_REPOSITORY_URL: ${GIT_REPOSITORY_URL}"
+export GIT_BRANCH=$(git symbolic-ref --short -q HEAD)
+echo "--- GIT_BRANCH: ${GIT_BRANCH}"
+export GIT_COMMIT_SHA=$(git rev-parse HEAD)
+echo "--- GIT_COMMIT_SHA: ${GIT_COMMIT_SHA}"
+export GIT_COMMIT_SHORT_SHA=$(git rev-parse --short HEAD)
+echo "--- GIT_COMMIT_SHORT_SHA: ${GIT_COMMIT_SHORT_SHA}"
+export GIT_COMMIT_TAG=$(git describe --tags --abbrev=0)
+echo "--- GIT_COMMIT_TAG: ${GIT_COMMIT_TAG}"
+export GIT_DIRTY=$(git_dirty)
+echo "--- GIT_DIRTY: ${GIT_DIRTY}"
 
 for service in services/${SERVICE:=*}/ ; do
 	build_image $(basename ${service})
--- services/anki-sync-server/images/bin/download-release.sh
+++ services/anki-sync-server/images/bin/download-release.sh
@@ -6,7 +6,10 @@ mkdir -p release
 
 cd release
 
-git clone https://github.com/ankicommunity/anki-sync-server
+export ANKISYNCD_GIT_REPOSITORY_URL=https://github.com/ankicommunity/anki-sync-server
+export ANKISYNCD_GIT_BRANCH=main
+git clone ${ANKISYNCD_GIT_REPOSITORY_URL}
+git checkout ${ANKISYNCD_GIT_BRANCH}
 
 mv anki-sync-server/src/* .
 
